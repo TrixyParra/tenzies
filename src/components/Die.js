76175a9243966cab089bea053d881cedb57a32f5
Trixy Parra <trixy.parra@gmail.com>
2023-05-19 04:57:46 -0700

    added conditional styling to the background color of the die based on whether the die is held or not

--- src/components/Die.js
+++ src/components/Die.js
@@ -2,7 +2,10 @@
 export default function Die(props) {
 
     return (
-        <div className="die-face">
+        <div 
+            className="die-face" 
+            style={{ backgroundColor: props.isHeld ? "#59E391" : "#fff" }}
+        >
             <h2 className="die-number">
                 {props.value}
             </h2>
