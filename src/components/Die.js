864ba10dacffdd92c29e2c57dca54e78b7d64000
Trixy Parra <trixy.parra@gmail.com>
2023-05-19 02:48:05 -0700

    created die component - die, number, props

---
+++ src/components/Die.js
@@ -0,0 +1,11 @@
+
+export default function Die(props) {
+
+    return (
+        <div className="die-face">
+            <h2 className="die-number">
+                {props.value}
+            </h2>
+        </div>
+    )
+}
